412d3b970ae9108103b3068549a74ca35a696343
rps <rps@gti.ssr.upm.es>
2015-03-05 16:39:31 +0100

    Now a file with the list of images is needed

--- calib2meshlab.py
+++ calib2meshlab.py
@@ -1,16 +1,30 @@
 import sys
 
+if len(sys.argv) != 5:
+  print "Wrong number of input parameter!"
+  print "Usage: " + sys.argv[0] + " <meshFileName> <calibrationFileName> <imageListFileName> <outputSceneFileName.mlp>"
+  exit()
+
 # Taking input arguments
 meshName = sys.argv[1]
 inName = sys.argv[2]
-outName = sys.argv[3]
+imageList = sys.argv[3]
+outName = sys.argv[4]
+
 
 outFile = open(outName, "w")
 inFile = open(inName, "r")
+imageListFile = open(imageList, "r")
 
 # First line indicates number of cameras and image resolutions
 line = inFile.readline().split()
 numCam = int(line[0])
+
+# Now we read the image list file with the name of every image
+imageNames = []
+for imageline in imageListFile:
+  imageline = imageline.rstrip("\n")
+  imageNames.append(imageline)
 
 #
 allMatrices = []
@@ -58,6 +72,7 @@ for i in range(0, numCam):
   extr = allMatrices[i][1]
   wid = allMatrices[i][3]
   hei = allMatrices[i][4]
+  imName = imageNames[i]
 
   # Position of the camera is inversed
   outFile.write("   <VCGCamera TranslationVector=\"%.16e %.16e %.16e\"" % (-float(pos[0]), -float(pos[1]), -float(pos[2])))
@@ -68,7 +83,8 @@ for i in range(0, numCam):
   outFile.write(" CenterPx=\"%d %d\" FocalMm=\"%.16e\"" % (int(centerX), int(centerY), focal))
   # The rotation matrix is all inversed except for the first vector
   outFile.write(" RotationMatrix=\"%.16e %.16e %.16e 0 %.16e %.16e %.16e 0 %.16e %.16e %.16e 0 0 0 0 1\"/>\n" % (float(extr[0]), float(extr[1]), float(extr[2]), -float(extr[3]), -float(extr[4]), -float(extr[5]), -float(extr[6]), -float(extr[7]), -float(extr[8])))
-  outFile.write("   <Plane semantic=\"\" fileName=\"%02d.png\"/>\n" % (i+1))
+  # outFile.write("   <Plane semantic=\"\" fileName=\"%02d.png\"/>\n" % (i+1))
+  outFile.write("   <Plane semantic=\"\" fileName=\"%s\"/>\n" % (imName))
   outFile.write("  </MLRaster>\n\n")
 
 
